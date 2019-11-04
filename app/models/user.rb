4ec4d6c53796c2e00b5c3733aec85aee1aa1ec74
Kim Nay <kchhuok@gmail.com>
2019-11-04 12:17:16 +0100

    modif

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
+require 'bcrypt'
 class User < ApplicationRecord
     belongs_to :city
     has_many :gossips
     has_many :comments
-   
-end
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+end 
